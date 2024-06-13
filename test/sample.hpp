9791dcb14bc153eb5c4d8d3f74ee4f8d2242d185
Marco Nikander <m4n1k.developer@gmail.com>
2024-06-13 16:49:01 +0200

    symbol set for testing

---
+++ test/sample.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <array>
+
+namespace land {
+
+std::array<char, 5U> symbols{'.', '+', '*', '@', '^'};
+std::array<char, 6U> invalid_symbols{'.', '+', '*', '@', '^', '#'};
+
+}
