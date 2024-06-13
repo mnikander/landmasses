41594497eeb02937d41c99489ab44a9e6864f92d
Marco Nikander <m4n1k.developer@gmail.com>
2024-06-13 16:49:19 +0200

    test index function

--- test/test_image.cpp
+++ test/test_image.cpp
@@ -3,6 +3,17 @@
 
 namespace {
 
+TEST(image, index)
+{
+    const land::Image<char, 3, 2> image{};
+    EXPECT_EQ(image.index(0, 0), 0);
+    EXPECT_EQ(image.index(0, 1), 1);
+    EXPECT_EQ(image.index(1, 0), 2);
+    EXPECT_EQ(image.index(1, 1), 3);
+    EXPECT_EQ(image.index(2, 0), 4);
+    EXPECT_EQ(image.index(2, 1), 5);
+}
+
 TEST(image, empty)
 {
     const land::Image<char, 0, 0> image{};
