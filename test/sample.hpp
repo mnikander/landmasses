f693a168d6cdb1b4ab4b4c1a8739eda99380a5de
Marco Nikander <m4n1k.developer@gmail.com>
2024-06-13 16:49:47 +0200

    more samples for testing

--- test/sample.hpp
+++ test/sample.hpp
@@ -4,7 +4,28 @@
 
 namespace land {
 
-std::array<char, 5U> symbols{'.', '+', '*', '@', '^'};
+std::array<char, 5U> symbols        {'.', '+', '*', '@', '^'};
 std::array<char, 6U> invalid_symbols{'.', '+', '*', '@', '^', '#'};
+
+// arrow or U-shapes are tricky for some pixel-kernel based clustering algorithms
+constexpr std::array<char, 9> up   {'.', '+', '.',
+                                    '+', '.', '+',
+                                    '+', '.', '+'};
+
+constexpr std::array<char, 9> down {'+', '.', '+',
+                                    '+', '.', '+',
+                                    '.', '+', '.'};
+
+constexpr std::array<char, 9> left {'.', '+', '+',
+                                    '+', '.', '.',
+                                    '.', '+', '+'};
+
+constexpr std::array<char, 9> right{'+', '+', '.',
+                                    '.', '.', '+',
+                                    '+', '+', '.'};
+
+constexpr std::array<char, 9> corners{'+', '.', '+',
+                                      '.', '.', '.',
+                                      '+', '.', '+'};
 
 }
